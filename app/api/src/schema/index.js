bb983b8a53f4b8cb9e156923e29708900468c9ec
Kolaxaisz <kolaxaisz@gmail.com>
2021-12-13 22:57:47 +0100

    5.6     Working with asynchronous functions

--- app/api/src/schema/index.js
+++ app/api/src/schema/index.js
@@ -15,12 +15,9 @@ const QueryType = new GraphQLObjectType({
     currentTime: {
       type: GraphQLString,
       resolve: () => {
-        return new Promise(resolve => {
-          setTimeout(()=>{
-            const isoString = new Date().toISOString()
-            resolve(isoString.slice(11,19))
-          },5000)
-        })
+        const isoString = new Date().toISOString()
+
+        return isoString.slice(11, 19)
       }
     },
     sumNumbersInRange: {
